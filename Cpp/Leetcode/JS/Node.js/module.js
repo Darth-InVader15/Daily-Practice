472770979906cf8f2ce5caf0013c5013ea34ce34
Piyush Ranjan Singh <95350374+Darth-InVader15@users.noreply.github.com>
2023-01-16 02:07:12 +0530

    //

---
+++ Cpp/Leetcode/JS/Node.js/module.js
@@ -0,0 +1,5 @@
+const xyz = require('./people'); //basically inherit krta h
+
+console.log(xyz); // returns empty if module.exports nhi hota to, else return value of module.exports
+//console.log(people); //throws error coz people iska part nhi h 
+
---
+++ Cpp/Leetcode/JS/Node.js/people.js
@@ -0,0 +1,6 @@
+const people = ['raj', 'ryuk', 'hiroshi', 'mario', 'ezio', 'haythm'];
+
+console.log(people);
+
+module.exports = "hello";
+module.exports = people;
