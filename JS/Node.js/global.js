c13af75a41e7ca7d733e1e5f2124cc496a96433a
Piyush Ranjan Singh <95350374+Darth-InVader15@users.noreply.github.com>
2023-01-16 02:10:12 +0530

    //

---
+++ JS/Node.js/global.js
@@ -0,0 +1,16 @@
+//Global Object
+
+//console.log(global);
+
+
+// global.setTimeout(() => {
+//     console.log('in the timeout'); // 3 sec baad print krega
+//     clearInterval(int); // 3 sec baad set interval wala bhi band hojaega
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('check'); // repeats in every interval
+// }, 1000);
+
+console.log(__dirname); // dir
+console.log(__filename); // dir with curr filename
---
+++ JS/Node.js/test.js
@@ -0,0 +1,10 @@
+// const name = 'Piyush';
+
+// console.log(name);
+
+const greet = (name) =>
+{
+    console.log('hello, ',(name));
+}
+greet('Raj');
+greet('Piyush');
